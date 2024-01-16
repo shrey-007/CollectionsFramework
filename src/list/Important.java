c392b79c335d43388e273c8a6e6ad49df2cb9a10
shreySaxena <shreyyerhs07@gmail.com>
2024-01-16 09:11:39 +0530

    added important thing

---
+++ src/list/Important.java
@@ -0,0 +1,15 @@
+package list;
+
+public class Important {
+    public static void main(String[] args) {
+        /*
+        * suppose you have an Arraylist<Integer>.
+        * ArrayList has two remove methods =
+        * 1)remove(int index)
+        * 2)remove(Integer object)
+        * toh agar suppose tumhe 4 hatana hai list se toh tum directly 4 paas kroge toh voh index samjhega, toh us 4 ko
+        * object mai convert krke paas kro
+        * list.remove(Integer.valueOf(4));
+        */
+    }
+}
